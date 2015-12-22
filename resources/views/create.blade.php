d6f3c2b7bff19ea8c0d5da37d70731a1200e3286
Cooper Sellers <cooper@fifostudios.com>
2015-12-22 16:28:33 -0600

    Adding check for existance of $errors

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,7 +10,7 @@
             <div class="panel-body pan">
 
 
-                @if (count($errors) > 0)
+                @if (isset( $errors ) && $errors && count($errors) > 0)
                     <div class="alert alert-danger">
                         <strong>Whoops!</strong> There were some problems with your input.<br><br>
                         <ul>
